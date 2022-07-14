66602da1ecfd65d53e094397ced452b5e4a401af
plncpeter <palenciocraft@gmail.com>
2022-07-14 19:22:46 -0300

    Initial commit

---
+++ A6/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace A6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtTitulo1 = new System.Windows.Forms.Label();
+            this.txtTitulo2 = new System.Windows.Forms.Label();
+            this.txtTitulo3 = new System.Windows.Forms.Label();
+            this.txtTitulo4 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnAcao1 = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnAcao2 = new System.Windows.Forms.Button();
+            this.btnAcao3 = new System.Windows.Forms.Button();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTitulo1
+            // 
+            this.txtTitulo1.AutoSize = true;
+            this.txtTitulo1.BackColor = System.Drawing.SystemColors.Control;
+            this.txtTitulo1.Font = new System.Drawing.Font("Harlow Solid Italic", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitulo1.ForeColor = System.Drawing.Color.PaleVioletRed;
+            this.txtTitulo1.Location = new System.Drawing.Point(324, 31);
+            this.txtTitulo1.Name = "txtTitulo1";
+            this.txtTitulo1.Size = new System.Drawing.Size(295, 40);
+            this.txtTitulo1.TabIndex = 0;
+            this.txtTitulo1.Text = "Be a Star Guardian!!";
+            // 
+            // txtTitulo2
+            // 
+            this.txtTitulo2.AutoSize = true;
+            this.txtTitulo2.Font = new System.Drawing.Font("Humanst521 BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitulo2.ForeColor = System.Drawing.Color.LightPink;
+            this.txtTitulo2.Location = new System.Drawing.Point(92, 146);
+            this.txtTitulo2.Name = "txtTitulo2";
+            this.txtTitulo2.Size = new System.Drawing.Size(99, 22);
+            this.txtTitulo2.TabIndex = 1;
+            this.txtTitulo2.Text = "Your Name";
+            // 
+            // txtTitulo3
+            // 
+            this.txtTitulo3.AutoSize = true;
+            this.txtTitulo3.Font = new System.Drawing.Font("Humanst521 BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitulo3.ForeColor = System.Drawing.Color.LightPink;
+            this.txtTitulo3.Location = new System.Drawing.Point(92, 196);
+            this.txtTitulo3.Name = "txtTitulo3";
+            this.txtTitulo3.Size = new System.Drawing.Size(135, 22);
+            this.txtTitulo3.TabIndex = 2;
+            this.txtTitulo3.Text = "Your Last Name";
+            // 
+            // txtTitulo4
+            // 
+            this.txtTitulo4.AutoSize = true;
+            this.txtTitulo4.Font = new System.Drawing.Font("Humanst521 BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitulo4.ForeColor = System.Drawing.Color.LightPink;
+            this.txtTitulo4.Location = new System.Drawing.Point(92, 292);
+            this.txtTitulo4.Name = "txtTitulo4";
+            this.txtTitulo4.Size = new System.Drawing.Size(81, 22);
+            this.txtTitulo4.TabIndex = 3;
+            this.txtTitulo4.Text = "Your Age";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(264, 150);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(184, 20);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(264, 200);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(184, 20);
+            this.txtSobrenome.TabIndex = 5;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(264, 296);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(184, 20);
+            this.txtAge.TabIndex = 6;
+            // 
+            // btnAcao1
+            // 
+            this.btnAcao1.Font = new System.Drawing.Font("Harlow Solid Italic", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcao1.ForeColor = System.Drawing.Color.PaleVioletRed;
+            this.btnAcao1.Location = new System.Drawing.Point(96, 452);
+            this.btnAcao1.Name = "btnAcao1";
+            this.btnAcao1.Size = new System.Drawing.Size(204, 64);
+            this.btnAcao1.TabIndex = 7;
+            this.btnAcao1.Text = "Transformate!";
+            this.btnAcao1.UseVisualStyleBackColor = true;
+            this.btnAcao1.Click += new System.EventHandler(this.btnAcao1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(616, 146);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 8;
+            // 
+            // btnAcao2
+            // 
+            this.btnAcao2.Location = new System.Drawing.Point(350, 452);
+            this.btnAcao2.Name = "btnAcao2";
+            this.btnAcao2.Size = new System.Drawing.Size(75, 29);
+            this.btnAcao2.TabIndex = 9;
+            this.btnAcao2.Text = "Clear";
+            this.btnAcao2.UseVisualStyleBackColor = true;
+            this.btnAcao2.Click += new System.EventHandler(this.btnAcao2_Click);
+            // 
+            // btnAcao3
+            // 
+            this.btnAcao3.Location = new System.Drawing.Point(350, 487);
+            this.btnAcao3.Name = "btnAcao3";
+            this.btnAcao3.Size = new System.Drawing.Size(75, 29);
+            this.btnAcao3.TabIndex = 10;
+            this.btnAcao3.Text = "Exit";
+            this.btnAcao3.UseVisualStyleBackColor = true;
+            this.btnAcao3.Click += new System.EventHandler(this.btnAcao3_Click);
+            // 
+            // pic1
+            // 
+            this.pic1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pic1.Image = global::A6.Properties.Resources.neutro;
+            this.pic1.Location = new System.Drawing.Point(619, 185);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(243, 258);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic1.TabIndex = 11;
+            this.pic1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(957, 578);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.btnAcao3);
+            this.Controls.Add(this.btnAcao2);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnAcao1);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtTitulo4);
+            this.Controls.Add(this.txtTitulo3);
+            this.Controls.Add(this.txtTitulo2);
+            this.Controls.Add(this.txtTitulo1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtTitulo1;
+        private System.Windows.Forms.Label txtTitulo2;
+        private System.Windows.Forms.Label txtTitulo3;
+        private System.Windows.Forms.Label txtTitulo4;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnAcao1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnAcao2;
+        private System.Windows.Forms.Button btnAcao3;
+        private System.Windows.Forms.PictureBox pic1;
+    }
+}
+
---
+++ A6/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcao1_Click(object sender, EventArgs e)
+        {
+            // Declaração das variaveis para o funcionamento do .exe:
+            string nome;
+            string sobrenome;
+            int ano;
+            int data = DateTime.Now.Year;
+            int conta;
+
+            nome = txtNome.Text ;
+            sobrenome = txtSobrenome.Text ;
+            ano = int.Parse(txtAge.Text) ;
+            conta = data - ano ;
+
+            if (conta >= 18 && conta <= 25)
+            {
+
+                lblResultado.Text = "Congratulations! You really are a Star Guardian, WOW";
+                lblResultado.ForeColor = Color.Goldenrod;
+                pic1.Image = Properties.Resources.tobe;
+                pic1.BackColor = Color.WhiteSmoke;
+            }
+            else if (conta >= 26)
+            {
+                lblResultado.Text = "Sorry, but you have a lot of experience!";
+                lblResultado.ForeColor = Color.Goldenrod;
+                pic1.Image = Properties.Resources.velho;
+            }
+            else if (conta <= 17)
+            {
+                lblResultado.Text = "Oh so sorry! But to be a Star Guardian needs more years";
+                lblResultado.ForeColor = Color.Goldenrod;
+                pic1.Image = Properties.Resources.menor;
+            }
+            else
+            {
+                lblResultado.Text = "Verique suas informações";
+                lblResultado.ForeColor = Color.Goldenrod;
+                pic1.Image = Properties.Resources.verifique;
+            }
+        }
+
+        private void btnAcao2_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            txtSobrenome.Text = "";
+            txtAge.Text = "";
+            pic1.Image = Properties.Resources.neutro;
+            lblResultado.Text = "";
+        }
+
+        private void btnAcao3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
